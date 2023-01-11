71133f66e9cded68dcbc812de43855729eddae2e
sarboleda72 <sarboleda72@misena.edu.co>
2023-01-10 21:42:49 -0500

    Se hacen ejercicios de estructuras de control

---
+++ mayor.py
@@ -0,0 +1,6 @@
+n1=float(input("Igrese número 1= "))
+n2=float(input("Ingrese el número 2= "))
+if n1>n2:
+    print(f"El número mayor es {n1}")
+else:
+    print(f"El número mayor es {n2}")
---
+++ multiplos_de_5_3.py
@@ -0,0 +1,5 @@
+n=float(input("Ingrese número= "))
+if n%3==0 and n%5==0:
+    print("El número es multiplo de 3 y 5")
+else:
+    print("El número no es multiplo de 3 y 5")
